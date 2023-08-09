45e651a59fa1fa77da002bc8087aea9964b60ede
Ranjan Srinivas <ranjan3101997@gmail.com>
2023-08-08 22:21:49 -0700

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
-import { GithubIcon, TwitterIcon } from 'lucide-react';
+import { GithubIcon, TwitterIcon } from "lucide-react";
 
 export default function Navbar() {
-	return (
-		<div className="flex flex-row w-screen justify-center gap-5 h-12 items-center m-4">
-			<a href="https://www.github.com/rkaahean" target="_blank">
-				<GithubIcon />
-			</a>
-			<a href="https://x.com/@rkaahean" target="_blank">
-				<TwitterIcon />
-			</a>
-		</div>
-	);
+  return (
+    <div className="flex flex-row w-screen justify-center gap-5 h-12 items-center m-4">
+      <a href="https://www.github.com/rkaahean" target="_blank">
+        <GithubIcon />
+      </a>
+      <a href="https://x.com/@rkaahean" target="_blank">
+        <TwitterIcon />
+      </a>
+    </div>
+  );
 }
