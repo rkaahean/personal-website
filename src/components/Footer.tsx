a5ff8cdb786a1acf8537a45a31363692a52e1a5c
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-04-29 07:29:03 +0400

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="absolute bottom-0 mb-5 text-stone-500">
+    <div className="relative bottom-0 mt-10 mb-5 text-stone-500">
       Source code for my website{" "}
       <a
         href="https://github.com/rkaahean/personal-website"
